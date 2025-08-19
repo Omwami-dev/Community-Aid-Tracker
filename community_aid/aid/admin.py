c9c35f7e219866b667ee549f78b748a0c1edf68d
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-19 20:07:22 +0300

    update admin.py

--- community_aid/aid/admin.py
+++ community_aid/aid/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
+# aid/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Project, Donation, Beneficiary, Volunteer
+
+# Register custom User with standard UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Add extra fields to the admin interface
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+# Register other models
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "start_date", "end_date", "created_by")
+    search_fields = ("title", "description", "status")
+    list_filter = ("status", "start_date", "end_date")
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ("donor", "project", "amount", "date")
+    search_fields = ("donor__username", "project__title")
+    list_filter = ("date",)
+
+@admin.register(Beneficiary)
+class BeneficiaryAdmin(admin.ModelAdmin):
+    list_display = ("name", "project", "contact_info")
+    search_fields = ("name", "project__title")
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ("user", "project", "role", "date_joined")
+    search_fields = ("user__username", "project__title", "role")
+    list_filter = ("date_joined",)
