be379c74b1e805c5493e229f9edef4b0c12dc56e
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-19 19:51:13 +0300

    update user model

--- community_aid/aid/models.py
+++ community_aid/aid/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
@@ -7,12 +7,14 @@ class User(AbstractUser):
 
     # Override ManyToMany fields with related_name to avoid clashes
     groups = models.ManyToManyField(
+        Group,
         related_name="custom_user_set",  # changed name
         blank=True,
         help_text="The groups this user belongs to.",
         verbose_name="groups",
     )
     user_permissions = models.ManyToManyField(
+        Permission,
         related_name="custom_user_permissions_set",  # changed name
         blank=True,
         help_text="Specific permissions for this user.",
