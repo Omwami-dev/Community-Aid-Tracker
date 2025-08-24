020127adc0e7bd09cf9ceadfa95569b4383dd4b8
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 20:56:52 +0300

    create DRF views in aid app

--- community_aid/aid/views.py
+++ community_aid/aid/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Donation, Beneficiary, Volunteer
+from .serializers import ProjectSerializer, DonationSerializer, BeneficiarySerializer, VolunteerSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class DonationViewSet(viewsets.ModelViewSet):
+    queryset = Donation.objects.all()
+    serializer_class = DonationSerializer
+
+class BeneficiaryViewSet(viewsets.ModelViewSet):
+    queryset = Beneficiary.objects.all()
+    serializer_class = BeneficiarySerializer
+
+class VolunteerViewSet(viewsets.ModelViewSet):
+    queryset = Volunteer.objects.all()
+    serializer_class = VolunteerSerializer
+
