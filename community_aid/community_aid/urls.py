baf14ff3e39e9480c79e4adfc7f74150baf54f61
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 21:05:26 +0300

    update community-Aid-Tracker urls

--- community_aid/community_aid/urls.py
+++ community_aid/community_aid/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from aid.views import ProjectViewSet, DonationViewSet, BeneficiaryViewSet, VolunteerViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'donations', DonationViewSet)
+router.register(r'beneficiaries', BeneficiaryViewSet)
+router.register(r'volunteers', VolunteerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/', include(router.urls)),
 ]
