03e6709eb598ce52173e404fc7320a6015df9cce
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 21:41:33 +0300

    update beneficiaries viewsets

--- community_aid/aid/views.py
+++ community_aid/aid/views.py
@@ -18,6 +18,7 @@ class BeneficiaryViewSet(viewsets.ModelViewSet):
     queryset = Beneficiary.objects.all()
     serializer_class = BeneficiarySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Public can view, only logged-in users can modify
+    
     def get_queryset(self):
         if self.request.user.is_staff:
             return Beneficiary.objects.all()
