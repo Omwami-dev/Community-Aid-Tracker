3807f7a5551fedb845dd2a39cb4eb6b74c296360
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 22:15:33 +0300

    update admin panel

--- community_aid/community_aid/settings.py
+++ community_aid/community_aid/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',  # For login via browser session
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',    # For basic username/password auth
     ],
     'DEFAULT_PERMISSION_CLASSES': [
--- community_aid/community_aid/urls.py
+++ community_aid/community_aid/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from aid.views import ProjectViewSet, DonationViewSet, BeneficiaryViewSet, VolunteerViewSet
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)
 router.register(r'donations', DonationViewSet)
@@ -27,5 +27,7 @@ router.register(r'volunteers', VolunteerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
