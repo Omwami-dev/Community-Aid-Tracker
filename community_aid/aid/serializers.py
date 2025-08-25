d4e3c83293339c155901810602239804806ae3e0
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-25 07:50:11 +0300

    update serializers

--- community_aid/aid/serializers.py
+++ community_aid/aid/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Project, Donation, Beneficiary, Volunteer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +13,7 @@ class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
         fields = '__all__'
+        read_only_fields = ['amount'] 
 
 class BeneficiarySerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +24,8 @@ class VolunteerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Volunteer
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff']
